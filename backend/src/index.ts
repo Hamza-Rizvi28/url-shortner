b25f782545974d580647d2f9fd7bc107da85fbef
Hamza-Rizvi28 <hamzarizvi110@gmail.com>
2024-08-25 01:22:58 +0500

    added shortened url creation post route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,22 @@
-import express, { Request, Response, Express } from "express";
+import express, { Request, Response, Express, json } from "express";
+import { createShortenedUrlMethod } from "./controllers/shortenedUrl.controllers";
 
 const app : Express = express();
 const port : Number = 8080;
+
+app.use(express.json());
 
 app.get("/", (req : Request, res : Response) => {
     res.json({"result": "Main get route"});
     res.statusCode = 200;
 });
+
+app.post("/url/create", async (req : Request, res : Response) => {
+    
+    const longUrl = req.body.longUrl;
+    const result = await createShortenedUrlMethod(longUrl);
+    res.json({"result": result});
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
