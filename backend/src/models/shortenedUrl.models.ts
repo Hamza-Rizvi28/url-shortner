feaf1c9bd9e1973d148b9bb8c4e0859498255a9f
Hamza-Rizvi28 <hamzarizvi110@gmail.com>
2024-08-25 01:20:41 +0500

    added queries related to shortened url

---
+++ backend/src/models/shortenedUrl.models.ts
@@ -0,0 +1,38 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const saveShortenedUrl = async (longUrl : string, hashedUrlKey : string) => {
+    return await prisma.shortenedUrl.create({
+        data: {
+          longUrl: longUrl,
+          shortUrl: `http://localhost/url/${hashedUrlKey}`,
+          key: hashedUrlKey,
+        },
+      });
+};
+
+export const urlExists = async (longUrl: string ) => {
+   return await prisma.shortenedUrl.findFirst({
+    select: {
+      longUrl: true,
+      shortUrl: true,
+      key: true,
+      createdAt: true
+    },
+    where: {
+      longUrl: longUrl
+    }
+   });
+};
+
+export const getLongUrlByKey = async (hashedUrlKey : string) => {
+  return await prisma.shortenedUrl.findFirst({
+    select: {
+      longUrl: true,
+    },
+    where: {
+      key: hashedUrlKey
+    }
+   });
+};
