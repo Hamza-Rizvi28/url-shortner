a7f907ca548cbd7fffff1bd7b52a75a2a5707eeb
Hamza-Rizvi28 <hamzarizvi110@gmail.com>
2024-09-21 21:23:07 +0300

    added prisma client singleton

--- backend/src/models/shortenedUrl.models.ts
+++ backend/src/models/shortenedUrl.models.ts
@@ -1,6 +1,4 @@
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
+import prisma from "./prisma";
 
 export const saveShortenedUrl = async (longUrl : string, hashedUrlKey : string) => {
     return await prisma.shortenedUrl.create({
