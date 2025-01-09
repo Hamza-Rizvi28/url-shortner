0ed0c8de3f0f5e038bfe376535a3ad89c1d20275
Hamza-Rizvi28 <hamzarizvi110@gmail.com>
2025-01-09 23:18:58 +0200

    added url result component to frontend

---
+++ frontend/src/components/ShortenedUrlResult.tsx
@@ -0,0 +1,56 @@
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import CopyToClipboardComponent from './CopyToClipboard';
+
+interface ShortenerUrlResultProps {
+    longUrl: string;
+    shortUrl: string;
+}
+
+const ShortenerUrlResult: React.FC<ShortenerUrlResultProps> = ({ longUrl, shortUrl }) => {
+
+    return (
+        <Box
+            sx={{
+                width: '100%',
+                maxWidth: 500,
+                margin: '0 auto',
+                mt: 5,
+                p: 3,
+                border: '1px solid #0000',
+                bgcolor: '#FFFFFF',
+                borderRadius: 3,
+                boxShadow: 3,
+                textAlign: 'center',
+            }}
+        >
+            <Typography variant="h6" align='left' color='string' gutterBottom>
+                Your Long URL
+            </Typography>
+
+            <TextField
+                id="long-url"
+                variant="outlined"
+                fullWidth
+                required
+                value={longUrl}
+            />
+            <Typography variant="h6" align='left' color='string' paddingTop={3}>
+                Shortened URL
+            </Typography>
+
+            <TextField
+                id="short-url"
+                variant="outlined"
+                fullWidth
+                required
+                value={shortUrl}
+            />
+            <CopyToClipboardComponent copiedText={shortUrl}/>
+        </Box>
+
+    );
+}
+
+export default ShortenerUrlResult;
