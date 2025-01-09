149cebb9f1df7baae16fcabcb421d7863db46ed2
Hamza-Rizvi28 <hamzarizvi110@gmail.com>
2025-01-09 19:39:05 +0200

    init frontend project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import NavBar from "./components/NavBar"
+import UrlShortener from "./components/URLForm";
+
+function App() {
+
+ return (
+    <>
+        <NavBar/>
+        <UrlShortener/>
+    </>
+    
+ );
+}
+
+export default App
