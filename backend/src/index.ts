70c940b2f160a3c2f80eaaddeff2861b982b201e
Hamza-Rizvi28 <hamzarizvi110@gmail.com>
2025-02-16 23:32:09 +0200

    added signup and login routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,12 @@
-import express, { Request, Response, Express, json } from "express";
+import express, { Request, Response, Express } from "express";
 import { createShortenedUrlMethod, getLongUrl } from "./controllers/shortenedUrl.controllers";
 import { environment } from "./config";
 import { CORS_ORIGIN, HTTP_STATUS } from "./constants";
+import bcrypt from "bcrypt";
+import prisma from "./models/prisma";
+import { authenticateJwt } from "./authenticationMiddleware";
+import { generateJwtAccessToken, generateJwtRefreshToken } from "./utils";
+import { createUser, getUserByEmail } from "./models/user.model";
 
 const app : Express = express();
 const port : number = environment.PORT;
@@ -16,7 +21,61 @@ app.get("/", (request : Request, response : Response) => {
     response.status(HTTP_STATUS.OK).json({"result": "Main get route"});
 });
 
-app.post("/url/create", async (request : Request, response : Response) => {
+app.post("/auth/signup", async (request : Request, response : Response) => {
+    try {
+        const { email, username, password } = request.body;
+
+        const existingUser = await getUserByEmail(email);
+        if (existingUser) {
+            return response.status(HTTP_STATUS.BAD_REQUEST).json({ message: "User already exists" });
+        }
+        
+        const hashedPassword = await bcrypt.hash(password, 10);
+        await createUser(email, username, hashedPassword);
+        response.status(HTTP_STATUS.CREATED).json({ message: "User registered successfully" });
+    }
+    catch (error) {
+        console.error(error);
+        response.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({ message: "Something went wrong!"});
+    }
+});
+
+app.post("/auth/login", async (request : Request, response : Response) => {
+    try {
+        const { email, password } = request.body;
+        
+        const user = await prisma.user.findUnique({where : { email } });
+        if (!user) {
+            return response.status(HTTP_STATUS.BAD_REQUEST).json({ message: "Invalid email or password" });
+        }
+
+        const isPasswordSame = await bcrypt.compare(password, user.password);
+        if (!isPasswordSame) {
+            return response.status(HTTP_STATUS.BAD_REQUEST).json({ message: "Invalid email or password" });
+        }
+        
+        const userForSigning = { id: user.id, email: user.email };
+        const accessToken = generateJwtAccessToken(userForSigning);
+        const refreshToken = generateJwtRefreshToken(userForSigning);
+        
+        response.cookie("refreshToken", refreshToken, {
+            httpOnly: true,
+            secure: environment.NODE_ENV !== 'development',
+            maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+        });
+        response.status(200).json({
+            message: "Login successful",
+            accessToken,
+            user: { id: user.id, email: user.email, name: user.username },
+        });
+    }
+    catch (error) {
+        console.error(error);
+        response.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({ message: "Something went wrong!"});
+    }
+});
+
+app.post("/url/create", authenticateJwt ,async (request : Request, response : Response) => {
     try {
         const result = await createShortenedUrlMethod(request);
         const {message, ...filteredResult} = result;
