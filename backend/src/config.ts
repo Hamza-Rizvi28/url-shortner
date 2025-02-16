5f7e4006cd16a26194b4661237413d7e0564694f
Hamza-Rizvi28 <hamzarizvi110@gmail.com>
2025-02-16 23:26:07 +0200

    removed unused environment variables

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -4,9 +4,6 @@ dotenv.config();
 
 interface EnvVariables {
     PORT: number;
-    DB_NAME: string;
-    DB_USER: string;
-    DB_PASSWORD: string;
     NODE_ENV: string;
     DATABASE_URL: string;
     PROD_BASE_URL : string;
@@ -17,9 +14,6 @@ interface EnvVariables {
 const getEnvVariables = (): EnvVariables => {
     return {
         PORT: parseInt(process.env.PORT as string, 10) || 8080,
-        DB_NAME: process.env.DB_NAME as string,
-        DB_USER: process.env.DB_USER as string,
-        DB_PASSWORD: process.env.DB_PASSWORD as string,
         NODE_ENV: process.env.NODE_ENV as string || "development",
         DATABASE_URL: process.env.DATABASE_URL as string,
         PROD_BASE_URL: process.env.PROD_BASE_URL as string,
