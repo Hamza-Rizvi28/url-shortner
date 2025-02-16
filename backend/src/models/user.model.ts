f02f9db1b9fc75b16ca1da565afd73fa7a65b6af
Hamza-Rizvi28 <hamzarizvi110@gmail.com>
2025-02-16 23:20:39 +0200

    added user creation and getByEmail prisma queries

---
+++ backend/src/models/user.model.ts
@@ -0,0 +1,11 @@
+import prisma from "./prisma";
+
+export const getUserByEmail = async (email: string) => {
+    return await prisma.user.findUnique({ where: { email } });
+}
+
+export const createUser = async (email: string, username: string, hashedPassword: string) => {
+    return prisma.user.create({
+        data: {email: email, username: username, password: hashedPassword }
+    });
+}
