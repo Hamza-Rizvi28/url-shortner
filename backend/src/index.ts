56cbc02a5f1fdbed3d3f5ce30494748d6db6a7e2
Hamza-Rizvi28 <hamzarizvi110@gmail.com>
2025-02-18 23:00:48 +0200

    fix styling issues

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,27 +1,27 @@
-import express, { Request, Response, Express } from "express";
-import { environment } from "./config";
-import { CORS_ORIGIN, HTTP_STATUS } from "./constants";
-import cookieParser from "cookie-parser";
-import urlRouter from "./routes/url.routes";
-import authRouter from "./routes/auth.routes";
+import express, { Request, Response, Express } from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import urlRouter from './routes/url.route';
+import authRouter from './routes/auth.route';
+import { environment } from './config';
+import { CORS_ORIGIN, HTTP_STATUS } from './constants';
 
 const app : Express = express();
 const port : number = environment.PORT;
-const cors = require('cors');
 
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 app.use(cors({
-    origin: CORS_ORIGIN
+    origin: CORS_ORIGIN,
 }));
 
-app.get("/", (request : Request, response : Response) => {
-    response.status(HTTP_STATUS.OK).json({"result": "Main get route"});
+app.get('/', (request : Request, response : Response) => {
+    response.status(HTTP_STATUS.OK).json({ 'result': 'Main get route' });
 });
 
 app.use('/auth', authRouter);
 app.use('/url', urlRouter);
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`);
 });
